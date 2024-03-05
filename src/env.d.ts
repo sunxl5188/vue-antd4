0bc99d9298118294db771ecfc1b36a5ffbd6287c
sunxl <yyhj@qq.com>
2024-03-05 17:54:41 +0800

    环境变量提示

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-	readonly VITE_APP_TITLE: string
-	// 更多环境变量...
+	readonly VITE_TITLE: string
+	readonly VITE_API: string
+	readonly VITE_TARGET: string
 }
 
 interface ImportMeta {
