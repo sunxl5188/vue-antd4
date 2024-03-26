9ba39bafeff6c052ebda82a39156d66a3b7dee67
sunxl <yyhj@qq.com>
2024-03-26 18:10:54 +0800

    表格

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -1,7 +1,43 @@
 <template>
-	<div></div>
+	<a-table v-bind="table"></a-table>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts" name="MyTable">
+import { TableProps } from 'ant-design-vue'
+const table = reactive<TableProps>({
+	loading: false,
+	rowKey: 'id',
+	dataSource: [],
+	columns: [
+		{
+			title: '姓名',
+			dataIndex: 'name',
+			key: 'name'
+		},
+		{
+			title: '年龄',
+			dataIndex: 'age',
+			key: 'age'
+		},
+		{
+			title: '住址',
+			dataIndex: 'address',
+			key: 'address'
+		}
+	],
+	pagination: {}
+})
 
-<style scoped></style>
+onMounted(() => {
+	const dataArray = []
+	for (let i = 1; i < 100; i++) {
+		dataArray.push({
+			id: i.toString(),
+			name: '胡彦斌',
+			age: (Math.random() * 10000).toFixed(0),
+			address: '西湖区湖底公园1号'
+		})
+	}
+	table.dataSource = dataArray
+})
+</script>
